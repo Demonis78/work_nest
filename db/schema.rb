9e4eb67339136126d7b06ac440b7078f2258677c
Demonis78 <szejm@fejm.pl>
2024-12-06 15:28:57 +0100

    Add Devise-based user profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_03_074743) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_19_120200) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -59,6 +59,18 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_03_074743) do
     t.index ["reservation_id", "variant_id"], name: "index_reservations_variants_on_reservation_id_and_variant_id", unique: true
     t.index ["reservation_id"], name: "index_reservations_variants_on_reservation_id"
     t.index ["variant_id"], name: "index_reservations_variants_on_variant_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   create_table "variants", force: :cascade do |t|
